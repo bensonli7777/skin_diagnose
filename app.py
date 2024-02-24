2b4ddd80263848e92061ea7e0eb58401a86d675b
bensonli7777 <bensonli7777@gmail.com>
2024-02-25 02:31:19 +0800

    修正端口錯誤

--- app.py
+++ app.py
@@ -34,4 +34,5 @@ if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
 if __name__ == '__main__':
-    app.run(port=8000, debug=True)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
